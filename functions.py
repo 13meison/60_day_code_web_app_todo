fefadd641e6297220e7a07978f55f2ede1603c87
13mei <mamaev.n@promail366.com>
2023-08-09 09:02:44 +0300

    FEAT: basically app done

---
+++ functions.py
@@ -0,0 +1,14 @@
+FILEPATH = 'todos.txt'
+
+
+def get_todos(path=FILEPATH) -> list:
+    """" Get data from file and return the list"""
+    with open(path, 'r') as file:
+        todos_local = file.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, path=FILEPATH):
+    """ Write data-list to file"""
+    with open(path, 'w') as file:
+        file.writelines(todos_arg)
